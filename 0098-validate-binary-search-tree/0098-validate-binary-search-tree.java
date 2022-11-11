a35bbc9abcac1de90a841e38194438831acf7bae
Shivam Singla <shivamsingla960@gmail.com>
2022-11-11 19:19:46 +0530

    Time: 5 ms (10.98%), Space: 44.7 MB (5.87%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,36 +14,18 @@
  * }
  */
 class Solution {
+    List<Integer> list = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root==null) {
-            List<Integer> f =new LinkedList<Integer>();
-            return f;
+        if(root == null)
+            return list;
+        
+        else
+        {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
         }
-        else {
-            List<Integer> left= inorderTraversal(root.left);
-            List<Integer> right= inorderTraversal(root.right);
-            List<Integer> f =new LinkedList<Integer>();
-            if(left==null&& right==null){        
-                f.add(root.val);
-                return f;
-            }
-            else if(left==null){
-                f.add(root.val);
-                f.addAll(right);
-                return f;
-            }
-            else if (right ==null){
-                f.addAll(left);
-                f.add(root.val);
-                return f;
-            }
-             else {
-                f.addAll(left);
-                f.add(root.val);
-                f.addAll(right);
-                return f;
-            }
-        }
+        return list;
     }
     public boolean isValidBST(TreeNode root) {
         List<Integer> a= inorderTraversal(root);
